b1cd1b4019e3f34cc8ac07f8f2201993445a1a23
Shini Stha <pranilshrestha2016@gmail.com>
2023-01-04 21:32:22 +0545

    Fifth.Five Submission
    Smoll Change

--- College_Assignments/LAB-5/smallest_in_array.c
+++ College_Assignments/LAB-5/smallest_in_array.c
@@ -2,15 +2,31 @@
 
 #include <stdio.h>
 
-int small(int n[]);
+int small(int n[], int x);
 
 int main()
 {
-    int n;
+    int n, i;
     printf("Enter how many numbers would you like to enter in the array: ");
     scanf("%d", &n);
 
     int arr[n];
-    // TO BE CONTINUED
+    for (i = 0; i < n; i++)
+    {
+        printf("Input in %d's place: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+    printf("%d is the smallest in array.\n", small(arr, n));
+    return 0;
+}
 
+int small(int n[], int x)
+{
+    int check = n[0], i;
+    for (i = 1; i < x; i++)
+    {
+        if (n[i] < check)
+            check = n[i];
+    }
+    return check;
 }
