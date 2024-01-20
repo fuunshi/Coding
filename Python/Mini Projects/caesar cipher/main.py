7c8c340a607541f61740bb73f21070bbe8ea807a
fuunshi <pranilshrestha2016@gmail.com>
2024-01-20 21:32:32 +0545

    Bug Fix and Added Features

--- Python/Mini Projects/caesar cipher/main.py
+++ Python/Mini Projects/caesar cipher/main.py
@@ -1,7 +1,15 @@
 def main():
     welcome()
-    prompt()
-    print(encrypt('hehe', 4))
+    while True:
+        prompted = prompt()
+        if prompted[0] == 'e':
+            result = encrypt(prompted[1], prompted[2])
+        else:
+            result = decrypt(prompted[1], prompted[2])
+        print(f"Result: { result }")
+        check = input("Would you like to continue y/n? ").lower()
+        if check != "y":
+            break
 
 def welcome():
     print(f"""
@@ -11,10 +19,25 @@ def welcome():
           """)
     
 def prompt():
-    choice = input("Would you like to encrypt(e) or decrypt(d): ").lower()
+    while True:
+        choice = input("Would you like to encrypt (e) or decrypt (d): ").lower()
+
+        if choice in ['e', 'd']:
+            break
+        else:
+            print("Invalid input. Please enter 'e' for encrypt or 'd' for decrypt.")
+
     choice_str = "encrypt" if choice == 'e' else "decrypt"
-    string = input(f"What message would you like to {choice_str}")
-    shift = int(input("What is the shift number: "))
+    string = input(f"What message would you like to {choice_str}: ")
+
+    while True:
+        try:
+            shift = int(input("What is the shift number: "))
+            break
+        except ValueError:
+            print("Invalid input. Please enter a valid integer for the shift.")
+
+
     return (choice, string, shift)
 
 def encrypt(str, shift):
